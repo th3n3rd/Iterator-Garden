a5a7689d4ee65f0fd3556b54cf4b03b980c27fd8
hakre <hanskrentel@yahoo.de>
2013-10-24 17:06:00 +0200

    Added DOMElementFilter

---
+++ src/DOM/DOMElementFilter.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Iterator-Garden
+ */
+
+/**
+ * Class DOMElementFilter
+ */
+class DOMElementFilter extends FilterIterator
+{
+    private $tagName;
+
+    public function __construct(DOMNodeList $nodeList, $tagName = NULL)
+    {
+        $this->tagName = $tagName;
+        parent::__construct(new IteratorIterator($nodeList));
+    }
+
+    /**
+     * @return bool true if the current element is acceptable, otherwise false.
+     */
+    public function accept()
+    {
+        $current = $this->getInnerIterator()->current();
+
+        if (!$current instanceof DOMElement) {
+            return FALSE;
+        }
+
+        return $this->tagName === NULL || $current->tagName === $this->tagName;
+    }
+}
